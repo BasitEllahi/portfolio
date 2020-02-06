5f3c5553f874aa0819b94ff38aeacb14b43d334a
Basit <Pk103_basit@hotmail.com>
2020-02-06 17:37:35 +0100

    Added changes

--- src/pages/work.js
+++ src/pages/work.js
@@ -32,22 +32,19 @@ const MainSection = styled.div`
   flex-direction: column;
   height: 100%;
   margin-top: 4rem;
-  width: 100%;
   justify-content: center;
   align-items: center;
 `
 
 const ProjectBox = styled.div`
-  display: grid;
-  flex-direction: row;
+  display: flex;
+  flex-direction: column;
   align-items: center;
-  grid-template-columns: unset;
   width: 80%;
   justify-content: center;
   transition: transform 0.3s;
   will-change: transform;
   ${media.desktop`
-    grid-template-columns: 50rem;
     justify-content: center;
   `};
 `
@@ -55,7 +52,6 @@ const ProjectBox = styled.div`
 const Project = styled.div`
   display: flex;
   flex-direction: column;
-  width: 43rem;
   height: 100%;
   margin-top: 2rem;
   margin-bottom: 2rem;
@@ -68,13 +64,13 @@ const scale = keyframes`
     transform: scale(1);
   }
   60% {
-    transform: scale(27.9);
+    transform: scale(22.9);
   }
   80% {
-    transform: scale(27.1);
+    transform: scale(22.1);
   }
   100% {
-    transform: scale(27.1);
+    transform: scale(22.1);
   }
 `
 const scaleBack = keyframes`
@@ -89,7 +85,16 @@ const scaleBack = keyframes`
     transform: scale(1);
   }
   0% {
-    transform: scale(26.1);
+    transform: scale(22.1);
+  }
+`
+
+const stroke = keyframes`
+  from {
+      stroke-dashoffset: 1000;
+  }
+  to {
+      stroke-dashoffset: 0;
   }
 `
 
@@ -99,7 +104,7 @@ const ProjectBack = styled.div`
   width: 1rem;
   height: 1rem;
   border-radius: 50%;
-  background-color: black;
+  background-color: #176bfc;
   position: absolute;
   bottom: 50%;
   left: 5%;
@@ -111,6 +116,42 @@ const ProjectBack = styled.div`
   :hover {
   }
 `
+const Arrow = styled.svg`
+  display: flex;
+  flex-direction: column;
+  width: 3rem;
+  height: 3rem;
+  position: absolute;
+  bottom: 50%;
+  left: 5%;
+  right: 40%;
+  top: 70%;
+  z-index: 1;
+  fill: none;
+  transition: all 600ms ease;
+  & .stroke-1 {
+    stroke-dashoffset: 1000;
+    stroke-dasharray: 1000;
+    fill: none;
+    stroke: white;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    stroke-width: 3px;
+  }
+
+  & .stroke-2 {
+    stroke-dashoffset: 1000;
+    stroke-dasharray: 1000;
+    fill: none;
+    stroke: white;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    stroke-width: 3px;
+  }
+  :hover {
+  }
+`
+
 const ImgBox = styled.div`
   display: flex;
   flex-direction: row-reverse;
@@ -128,6 +169,26 @@ const ImgBox = styled.div`
     > img {
       filter: grayscale(0);
     }
+    > svg {
+      & .stroke-1 {
+        animation: 1.7s ${stroke} cubic-bezier(0.51, 0.05, 0.38, 1.01) forwards;
+        animation-fill-mode: forwards;
+        animation-delay: 0.1s;
+      }
+
+      & .stroke-2 {
+        animation: 1.9s ${stroke} cubic-bezier(0.51, 0.05, 0.38, 1.01) forwards;
+        animation-fill-mode: forwards;
+        animation-delay: 0.1s;
+      }
+    }
+  }
+  & :after {
+    display: flex;
+    width: 4rem;
+    height: 4rem;
+    border-radius: 50%;
+    background-color: green;
   }
 `
 
@@ -141,11 +202,11 @@ const Image = styled.img`
 `
 
 const Title = styled(AniLink)`
-  color: white;
+  color: #2b2d2c;
   font-size: 3rem;
   margin-top: 0.5rem;
   line-height: 1;
-  font-family: Black;
+  font-family: bebas;
   font-weight: 200;
   text-align: center;
   -webkit-text-decoration: none;
@@ -155,7 +216,7 @@ const Title = styled(AniLink)`
   left: -3rem;
   top: 3rem;
   text-shadow: 0px 1px 4px rgba(150, 150, 150, 0.36);
-  background-color: #fc1c1c;
+  background-color: #ff0;
   padding: 1rem;
 `
 const UnderTitle = styled.span`
@@ -168,31 +229,6 @@ const UnderTitle = styled.span`
   margin-bottom: 0.3rem;
 `
 
-const Description = styled.p`
-  color: ${colors.darkGrey};
-  font-size: 0.8rem;
-  line-height: 1.2;
-  font-family: ${fonts.helvetica};
-  font-weight: 200;
-  text-align: center;
-  margin-top: 0.5rem;
-`
-
-const InfoTitle = styled.div`
-  color: black;
-  font-size: 3rem;
-  margin-top: 1rem;
-  margin-bottom: 1.5rem;
-  line-height: 1;
-  font-family: ${fonts.acumin};
-  display: flex;
-  jusfity-content: center;
-
-  & span {
-    color: ${colors.main};
-    margin-right: 0.5rem;
-  }
-`
 const slideData = [
   {
     index: 0,
@@ -248,6 +284,18 @@ const SecondPage = props => {
                 <ImgBox>
                   <Image src={detail.cover.fluid.src} alt="de kreun" />
                   <ProjectBack />
+                  <Arrow viewBox="0 0 72 35" width="4rem" height="4rem">
+                    <path
+                      className="stroke-1"
+                      d="M7.5 16.5h41"
+                      id="arrow_svg__line"
+                    />
+                    <path
+                      className="stroke-2"
+                      d="M47.81 4.81l15 11.59a.61.61 0 010 .82l-15 11.59"
+                      id="arrow_svg__line2"
+                    />
+                  </Arrow>
                 </ImgBox>
                 <Title
                   paintDrip
