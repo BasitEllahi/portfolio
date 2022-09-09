05b8c9278e926712c3cde342bdf35b9f5d24ab5a
Basit <basit@superkraft.be>
2022-09-09 14:31:10 +0200

    added animation on homepage svg

--- src/components/_sections/home/Illustration.js
+++ src/components/_sections/home/Illustration.js
@@ -1,7 +1,10 @@
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, createRef } from "react"
+import lottie from "lottie-web"
 import styled from "styled-components"
 
 import svgIllustration from "../../../assets/web-illustration.svg"
+
+import jsonData2 from "../../../assets/json/after_effects_animation.json"
 
 const ImageContainer = styled.div`
   display: flex;
@@ -14,11 +17,31 @@ const Image = styled.img`
   width: 100%;
   height: 100%;
 `
+const Logocontainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`
 
 const Illustration = () => {
+  const animationContainer = createRef()
+
+  useEffect(() => {
+    const anim = lottie.loadAnimation({
+      container: animationContainer.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: jsonData2,
+    })
+
+    return () => anim.destroy() // optional clean up for unmounting
+  }, [])
+
   return (
     <ImageContainer>
-      <Image src={svgIllustration} />
+      {/* <Image src={svgIllustration} /> */}
+      <Logocontainer ref={animationContainer} />
     </ImageContainer>
   )
 }
